93426bcae89c20986f9d152025d66c89018207ed
wayfarer <genia.sushko@gmail.com>
2015-02-10 13:01:27 +0200

    Modified PrimeDividers. Added some tests.

--- BionicProject/src/hometasks/PrimeDividers.java
+++ BionicProject/src/hometasks/PrimeDividers.java
@@ -5,23 +5,23 @@ import java.util.List;
 
 public class PrimeDividers {
 	public static void main(String[] args) {
-		int n = 1456;
+		int n = 40;
+		System.out.println(findPrimeDividers(n).toString());
+	}
+	
+	public static List<Integer> findPrimeDividers(int n) {
 		List<Integer> prime = new ArrayList<>();
-
 		List<Integer> div = findDividers(n);
-
+		
 		for (Integer num : div) {
 			if (isPrime(num)) {
 				prime.add(num);
 			}
 		}
-
-		for (Integer num : prime) {
-			System.out.println(num);
-		}
-
+		
+		return prime;
 	}
-
+	
 	public static boolean isPrime(Integer num) {
 		if (num == 2) {
 			return true;
@@ -34,19 +34,19 @@ public class PrimeDividers {
 			return true;
 		}
 	}
-
+	
 	public static List<Integer> findDividers(int n) {
 		List<Integer> list = new ArrayList<>();
 		if (n % 2 == 0) {
 			list.add(new Integer(2));
 		}
-
+		
 		for (int i = 3; i <= n; i += 2) {
 			if (n % i == 0) {
 				list.add(new Integer(i));
 			}
 		}
-
+		
 		return list;
 	}
 }
---
+++ BionicProject/src/tests/ExpRowTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import static org.junit.Assert.*;
+import hometasks.ExpRow;
+import org.junit.Test;
+
+public class ExpRowTest {
+	@Test
+	public void test10() {
+		test(10);
+	}
+	
+	@Test
+	public void test100() {
+		test(100);
+	}
+	
+	@Test
+	public void test_m10() {
+		System.out.println(ExpRow.expSum(-10));
+		test(-10);
+		
+	}
+
+
+	private void test(double x) {
+		assertEquals(1.0, Math.exp(x) / ExpRow.expSum(x), 1e-5);
+	}
+
+}
