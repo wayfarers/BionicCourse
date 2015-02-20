eec4f1209b7d21d56f6bbf5e2dba65f1b1f1958c
wayfarer <genia.sushko@gmail.com>
2015-02-20 20:49:15 +0200

    Another tasks with deposit.

---
+++ BionicProject/src/lesson07/BarrierDepositTest.java
@@ -0,0 +1,35 @@
+package lesson07;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.LocalDate;
+
+import org.junit.Test;
+
+public class BarrierDepositTest {
+
+	@Test
+	public void test() {
+		BarrierDeposit dep = new BarrierDeposit(15, 1000, LocalDate.of(2012, 9, 8), 20);
+		assertEquals(8.2, dep.getInterest(), 0);
+	}
+	
+	@Test
+	public void test2() {
+		BarrierDeposit dep = new BarrierDeposit(15, 60000, LocalDate.of(2012, 9, 8), 30);
+		assertEquals(786.89, dep.getInterest(), 0);
+	}
+	
+	@Test
+	public void test3() {
+		BarrierDeposit dep = new BarrierDeposit(15, 60000, LocalDate.of(2014, 9, 8), 30);
+		assertEquals(789.04, dep.getInterest(), 0);
+	}
+	
+	@Test
+	public void test4() {
+		BarrierDeposit dep = new BarrierDeposit(15, 100001, LocalDate.of(2014, 5, 12), 180);
+		assertEquals(8383.65, dep.getInterest(), 0);
+	}
+
+}
--- BionicProject/src/tests/SimpleDepositTest.java
+++ BionicProject/src/tests/SimpleDepositTest.java
@@ -13,25 +13,24 @@ public class SimpleDepositTest {
 	@Test
 	public void test() {
 		SimpleDeposit dep = new SimpleDeposit(15, 1000, LocalDate.of(2012, 9, 8), 20);
-		assertEquals(8.2, dep.getInterest(), 1e-2);
+		assertEquals(8.2, dep.getInterest(), 0);
 	}
 	
 	@Test
 	public void test2() {
 		SimpleDeposit dep = new SimpleDeposit(15, 1000, LocalDate.of(2012, 9, 8), 180);
-		assertEquals(73.84, dep.getInterest(), 1e-2);
+		assertEquals(73.84, dep.getInterest(), 0);
 	}
 	
 	@Test
 	public void test3() {
 		SimpleDeposit dep = new SimpleDeposit(15, 1000, LocalDate.of(2014, 9, 8), 20);
-		assertEquals(8.22, dep.getInterest(), 1e-2);
+		assertEquals(8.22, dep.getInterest(), 0);
 	}
 	
 	@Test
 	public void test4() {
 		SimpleDeposit dep = new SimpleDeposit(15, 1000, LocalDate.of(2014, 9, 8), 180);
-		assertEquals(73.97, dep.getInterest(), 1e-2);
+		assertEquals(73.97, dep.getInterest(), 0);
 	}
-
 }
