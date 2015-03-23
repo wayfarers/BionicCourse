a6023dd5b45d70c68e8f98b38ac0b29e3e4f68da
wayfarer <genia.sushko@gmail.com>
2015-03-23 20:13:17 +0200

    Added DividersCount by threads

--- BionicProject/src/concurrency/ThreadsMain.java
+++ BionicProject/src/concurrency/ThreadsMain.java
@@ -2,7 +2,7 @@ package concurrency;
 
 public class ThreadsMain {
 	public static void main(String[] args) {
-		Thread t1 = new ThreadExample();
+		Thread t1 = new Thread(new ThreadExample());
 		Thread t2 = new Thread(new RunnableExample(t1));
 		
 		t1.start();
